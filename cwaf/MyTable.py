db047ba450511aed63350467791644670e2a87f8
jumb0 <35896884+jumb01@users.noreply.github.com>
2018-03-05 14:06:58 +0000

    Add files via upload
    first upload. undergoing development

---
+++ cwaf/MyTable.py
@@ -0,0 +1,193 @@
+# coded by jumb0
+from platform import system
+
+if system() == "Windows":
+	print("Sorry, this program works with Linux only")
+else:
+	from col import Colours as C
+
+
+# within numberless method, self.defaultSpacing should be 3
+# new objective: find logical errors
+# think about adding append option, for appending to a list while a list table has already been created
+
+class Table(object):
+	"""
+	Create your own styled tables, numbered for menus, or non-numbered possibly for general information
+	Instantiation is as follows:
+	You could do T = MyTable.Table(["any", "list", "data", "..."], 2)
+	T.make_table()	or T.numberless_table() which will provide you with the default set table & colours
+				---OR (for customization of tables/menus)---
+	T = MyTable.Table(["any", "list", "data", "..."], 2) # pass in a list of data; and specify the number of prefered columns
+	T.title("Example of Use")		# title, style and colour methods are optional, each require some argument input
+	T.style("|", "=", "#")			# first=verical border, second=horizontal, third[optional]=edges
+	T.colour("\033[1;31m", "\033[0;32m")	# an easier option for colour is to import col, for ease
+	T.make_table()	or T.numberless_table() # either of these will print your table to screen
+	"""
+
+	def __init__(self, data, column):
+		self.column = column
+		self.data = data
+		self.Max_Length = 0
+		for item in self.data:
+			if len(item) > self.Max_Length:
+				self.Max_Length = len(item)
+		self.addon = None
+		self.row = 0
+		if self.column == 1:
+			self.row = len(self.data)
+		else:
+			for i in range(len(self.data)):
+				if i % self.column == 0:
+					self.row += 1
+			#colour and styling variables
+		self.borders = C.darkgrey; self.text = C.cyan
+		self.defaultStyle = ["|", "-", "+"]
+		self.defaultSpacing = 6
+		self.number_colour = C.lightgrey
+
+	def title(self, t):
+		print("-"*self.column + "[", t, "]" + "-"*self.column)
+
+	def colour(self, **kwargs):
+		"""
+		keyword arguments include: border | text | number
+		you should assign a colour to the specified keyword arg, e.g:
+		example.colour(borders="\033[0;31m"...)
+		example.colour(number="colour goes here", text="colour goes here")
+		-> arguments needn't be ordered, nor do they have to all be selected
+		together. Each argument could be processed individually.
+		"""
+		if len(kwargs)==1:
+			try:
+				self.borders = kwargs["border"]
+				return(None)
+			except: pass
+			try:
+				self.text = kwargs["text"]
+				return(None)
+			except: pass
+			try:
+				self.number_colour = kwargs["number"]
+				return(None)
+			except: pass
+		elif len(kwargs)==2:
+			try:
+				self.borders = kwargs["border"]
+				self.text = kwargs["text"]
+				return(None)
+			except: pass
+			try:
+				self.borders = kwargs["border"]
+				self.number_colour = kwargs["number"]
+				return(None)
+			except: pass
+			try: 
+				self.text = kwargs["text"]
+				self.number_colour = kwargs["number"]
+				return(None)
+			except: pass
+		elif len(kwargs)==3:
+			try:
+				self.borders = kwargs["border"]
+				self.text = kwargs["text"]
+				self.number_colour = kwargs["number"]
+				return(None)
+			except: pass
+		else:
+			self.borders = C.blue
+			self.text = C.orange
+			self.number_colour = C.green  # possibly add randomly selected colouring if args not specified?
+
+	def style(self, *args):
+		if len(args) > 2:
+			self.defaultStyle = [args[0], args[1], args[2]]
+		else:
+			self.defaultStyle = [args[0], args[1], "+"]
+
+	def make_table(self):
+		self.defaultSpacing = 6
+		for n in range(self.column):
+			if (n > 0) and (n < self.column):
+				print("{}".format(self.borders) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3)+ "{}".format(C.end), end="",)
+			elif (n == 0 or n+1 == self.column):
+				print("{}{}".format(self.borders, self.defaultStyle[2]) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
+		print("{}{}{}".format(self.borders, self.defaultStyle[2], C.end))
+		pipelines = 0
+		counter = 0
+		counter2 = 1
+		for i in self.data:
+			self.addon = 1
+			try:
+				while len(i)+self.addon != self.Max_Length+1: 
+					self.addon += 1
+					continue
+				if counter % self.column == 0 and counter > 0:
+					print("")
+				if counter+1 < 10:
+					print("{}{}{} ".format(self.borders, self.defaultStyle[0], C.end), "{}{}.{} {}{}{}".format(self.number_colour, counter+1, C.end, self.text, i, C.end), " "*self.addon, "{}{}{}".format(self.borders, self.defaultStyle[0], C.end), end="",)
+				else:
+					print("{}{}{} {}{}.{} {}{}{}".format(self.borders, self.defaultStyle[0], C.end, self.number_colour, counter+1, C.end, self.text, i, C.end), " "*self.addon, "{}{}{}".format(self.borders, self.defaultStyle[0], C.end), end="",)
+				pipelines+=2
+			except:
+				pass
+			counter += 1; counter2 += 1
+		if len(self.data) % self.column != 0:
+			c = self.column 	#column
+			R = self.row		# row
+			P = R*2				# num of pipelines
+			oP = P*c 			# overall pipelines (that should exist)
+			r = oP - pipelines 	# num of remainder (pipelines missing)
+			for i in range(r//2):
+				print("{}{}".format(self.borders, self.defaultStyle[0]), " "*(self.defaultSpacing+self.Max_Length),  "{}{}".format(self.defaultStyle[0], C.end), end="")
+		print("")
+		for n in range(self.column):
+			if (n > 0) and (n < self.column):
+				print("{}".format(self.borders) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3)+ "{}".format(C.end), end="",)
+			elif (n == 0 or n+1 == self.column):
+				print("{}{}".format(self.borders, self.defaultStyle[2]) + self.defaultStyle[1]*(6+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
+		print("{}{}{}".format(self.borders, self.defaultStyle[2], C.end))
+
+
+	def numberless_table(self):
+		self.defaultSpacing = 3
+		for n in range(self.column):
+			if (n > 0) and (n < self.column):
+				print("{}".format(self.borders) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3)+ "{}".format(C.end), end="",)
+			elif (n == 0 or n+1 == self.column):
+				print("{}{}".format(self.borders, self.defaultStyle[2]) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
+		print("{}{}{}".format(self.borders, self.defaultStyle[2], C.end))
+		pipelines = 0
+		counter = 0
+		counter2 = 1
+		for i in self.data:
+			self.addon = 1
+			try:
+				while len(i)+self.addon != self.Max_Length+1: 
+					self.addon += 1
+					continue
+				if counter % self.column == 0 and counter > 0:
+					print("")
+				if counter+1 < 10:
+					print("{}{}{} ".format(self.borders, self.defaultStyle[0], C.end), "{}{}{}".format(self.text, i, C.end), " "*self.addon, "{}{}{}".format(self.borders, self.defaultStyle[0], C.end), end="",)
+				else:
+					print("{}{}{}  {}{}{}".format(self.borders, self.defaultStyle[0], C.end, self.text, i, C.end), " "*self.addon, "{}{}{}".format(self.borders, self.defaultStyle[0], C.end), end="",)
+				pipelines+=2
+			except:
+				pass
+			counter += 1; counter2 += 1
+		if len(self.data) % self.column != 0:
+			c = self.column 	
+			R = self.row		
+			P = R*2				
+			oP = P*c 			
+			r = oP - pipelines 	
+			for i in range(r//2):
+				print("{}{}".format(self.borders, self.defaultStyle[0]), " "*(self.defaultSpacing+self.Max_Length),  "{}{}".format(self.defaultStyle[0], C.end), end="")
+		print("")
+		for n in range(self.column):
+			if (n > 0) and (n < self.column):
+				print("{}".format(self.borders) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3)+ "{}".format(C.end), end="",)
+			elif (n == 0 or n+1 == self.column):
+				print("{}{}".format(self.borders, self.defaultStyle[2]) + self.defaultStyle[1]*(self.defaultSpacing+self.Max_Length+3+(self.column-2))+ "{}".format(C.end), end="",)
+		print("{}{}{}".format(self.borders, self.defaultStyle[2], C.end))
---
+++ cwaf/cwaf.py
@@ -0,0 +1,214 @@
+import requests
+import urllib
+import titles
+import MyTable
+import random
+import time
+import sys
+import argparse
+import os
+import socket
+from col import Colours as c 
+
+is_target = None	# if argparsed or manually inputted set var to 1
+user_target = None
+title_shown = 0
+
+verbose_mode = 0
+
+options = ["target(s)", "nms", "wafw00f", "cwaf"]
+
+def show_options():
+	print("""
+	1 - set target, url or ipv4 addr to detect waf
+	2 - advanced nmap script against target
+	3 - wafw00f detection against target
+	4 - cwaf against target
+	clear (or c) - clear screen
+	options (or o) - show options
+	help (or h) - also shows options
+	quit (or q) - quit program
+	""")
+
+def error_input():
+	os.system("clear")
+	print(c.red, end="")
+	print("\n"*8)
+	print("[READ] Enter either 1, 2 or 3 in order to run the deisred method to detect a waf.")
+	print(c.end, end="")
+	time.sleep(3.8)
+	os.system("clear")
+	show_menu()
+
+def show_title():
+	print(c.lightgrey, end="")
+	print(titles.Title(), end="")
+	print(c.end)
+	global title_shown; title_shown = 1
+
+def show_menu():
+	menu = MyTable.Table(options, 2)
+	menu.colour(border=c.lightgrey, number=c.red, text=c.darkgrey)
+	menu.style("|", "~", "#")
+	menu.make_table()
+
+def user_selection():
+	valid_input = 0
+	invalid_input = 0
+	while valid_input != 1:
+		user_input = input("cwaf-opt-> ")
+		#if user input is return key
+		if user_input == "":
+			invalid_input += 1
+			if invalid_input == 7:
+				sys.exit(0)
+			print(c.red, end="")
+			if invalid_input < 7:
+				print("[E:] Enter a number which corresponds to your desired option.", c.end)
+			if invalid_input % 3 == 0:
+				error_input()
+		elif user_input == "1":
+			if is_target == 1:
+				target(user_target)
+			else: target()
+		elif user_input == "2":
+			nms(user_target)
+			valid_input = 1
+		elif user_input == "3":
+			wafw00f(user_target)
+			valid_input = 1
+		elif user_input == "4":
+			cwaf(user_target)
+			valid_input = 1
+		elif user_input[0] in ["c", "C"]:
+			os.system("clear")
+		elif user_input[0] in ["o", "O", "h", "H"]:
+			os.system("clear")
+			show_options()
+		elif user_input[0] in ["m", "M"]:
+			os.system("clear")
+			show_menu()
+		elif user_input[0] in ["q", "Q"]:
+			break
+		elif user_input[0] in ["t", "T"]:
+			print("target(s) currently set as: {}{}{}".format(c.red, user_target, c.end))
+		else: 
+			invalid_input += 1
+			if invalid_input == 7:
+				sys.exit(0)
+			print(c.red, end="")
+			if invalid_input < 7:
+				print("[E:] Enter a number which corresponds to your desired option.", c.end)
+			if invalid_input % 3 == 0:
+				error_input()
+
+def target(*args):
+	# write a file somewhere in the file system, to determine whether this program has been used before
+	# if it has, then dont show the prompt (how to parse multiple targets)
+	if os.path.exists("./.chk"):
+		pass
+	else:
+		chk = open("./.chk", "w")
+		chk.write("1\n")
+		print(c.cyan)
+		print("[In order to add multiple targets, simply add a target separated by a comma and a space.]")
+		print("e.g. example.com, something.com, somewhere.com || 192.168.44.33, books.org, 22.22.22.42")
+		print(c.end)
+	global user_target
+	is_set = 0
+	if len(args) < 1:
+		if is_set != 1:
+			while is_set != 1:
+				target = input("set-target-> ")
+				if target == "":
+					print(c.red, end="")
+					print("you must enter a target, (URL or IPv4) e.g. target.com or 192.168.88.776")
+					print(c.end, end="")
+				else:
+					is_set = 1
+			if "," in target:
+				targets = target.split(", ")
+				user_target = targets
+			else: user_target = target
+	else:
+		print("~> target(s) already set to {}{}{};".format(c.red, args[0], c.end), end=" ")
+		print("are you sure you want to change this [n]? ", end=" ")
+		y_or_n = input()
+		if y_or_n == "":
+			return None
+		elif y_or_n[0] in ["n", "N", "x", "X", "c", "C"]:
+			return None
+		elif y_or_n in ["y", "Y"]:
+			target = input("set-target-> " )
+			if "," in target:
+				targets = target.split(", ")
+				user_target = targets
+			else: user_target = target
+	print("~> target set to {}{}{}".format(c.red, user_target, c.end))
+	return None
+
+def nms(target):
+	print(target)
+
+def wafw00f(target):
+	print(target)
+
+def cwaf(target):
+	print(target)
+
+def main():
+	parser = argparse.ArgumentParser()
+	group = parser.add_mutually_exclusive_group()
+	parser.add_argument("--output", "-o", required=False, 
+		help="output results in a text file")
+	parser.add_argument("--verbose", "-v", 
+		help="increase output verbosity", action="store_true")
+	parser.add_argument("--proxy", "-p", type=str, required=False, 
+		help="specify a proxy server and port you would like to connect to (if you're using cwaf [opt:4])\
+		--proxy=\"172.443.26.26:8080\"")
+	parser.add_argument("--cwaf", action="store_true", 
+		help="initiate a cwaf detection scan without going through UI")
+	group.add_argument("--target", "-t", required=False, type=str, 
+		help="specify a target URL or IPv4 address")
+	group.add_argument("--targetfile", "-tf", required=False, 
+		help="specify a file with list of targets, which are separated by newline characters")
+	arg = parser.parse_args()		
+
+	if arg.target:
+		global is_target; is_target = 1
+		global user_target;
+		if "," in arg.target:
+			targets = arg.target.split(", ")
+			user_target = targets
+		else: 
+			user_target = arg.target
+
+	if arg.targetfile:
+		target_list = []
+		with open(arg.targetfile, "r") as tf:
+			for target in tf.readlines():
+				target_list.append(target.strip("\n"))
+		user_target = target_list
+
+	if arg.verbose:
+		# increase verbosity
+		pass
+
+	if arg.output:
+		#write output to output file
+		pass 
+
+	if arg.proxy:
+		#split by ":" to verify host and port
+		pass
+
+	if arg.cwaf:
+		pass
+
+	if title_shown == 0:
+		show_title()
+	show_menu()
+	user_selection()
+
+if __name__ == "__main__":
+	main()  
